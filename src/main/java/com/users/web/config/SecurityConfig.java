aa9a7f6e01c2dfffd031f42a345662265b86537d
nigolapalanona <152204169+NigolaPaLaNona@users.noreply.github.com>
2024-01-15 16:39:30 -0300

    apply BasicAuthenticationFilter

--- src/main/java/com/users/web/config/SecurityConfig.java
+++ src/main/java/com/users/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.users.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,7 +11,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
