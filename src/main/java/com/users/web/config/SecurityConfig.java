5e551fc8e81ac72c8c3e9acbadacd9c6f62da5aa
GnicoDev <gnicolasalvarezf@gmail.com>
2025-03-06 14:08:09 -0300

    feat(security): Allow USER role to access own profile data
    - Adjusted SecurityConfig to permit USER role to access their own profile data using the endpoint /api/users/{username}.
    - Updated permissions to balance accessibility and security for USER and ADMIN roles.
    
    This commit allows users to view and edit their own profile data while maintaining secure access controls.

--- src/main/java/com/users/web/config/SecurityConfig.java
+++ src/main/java/com/users/web/config/SecurityConfig.java
@@ -36,9 +36,10 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users/{username}").hasAnyRole("ADMIN","USER")
                         .requestMatchers(HttpMethod.GET, "/api/users/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/**").hasAnyRole("ADMIN","USER")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
